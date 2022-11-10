72fd23e1a786a452453196f8d128196853a02548
Ryan Rickey <rtrickey@ucsd.edu>
2022-11-10 11:35:58 -0800

    edited fractal_turtle to make random

--- a1/fractal_turtle.py
+++ a1/fractal_turtle.py
@@ -1,24 +1,36 @@
 # code from https://pythonguides.com/fractal-python-turtle/
 from turtle import *
 import turtle
+import random
+from PIL import Image
 
 speed('fastest')
 
 right(-90)
-angle = 30
+angle = 30 # randomize
 
 def yaxis(size, lvl):
     if lvl > 0:
+        angle = 30 + random.randint(0, 60)
         colormode(255)
-        pencolor(255 // lvl**2, 255 // lvl, 255 // (lvl*3 % 255))
+        pencolor(60, 255, 0)
         forward(size)
         right(angle)
         yaxis(0.8 * size, lvl - 1)
-        pencolor(0, 255 // lvl, 0)
+        pencolor(0, 255 - (lvl * random.randint(1, 10)), 0)
         lt(2 * angle)
         yaxis(0.8 * size, lvl-1)
-        pencolor(0, 255 // lvl, 0)
+        pencolor(0, 255 - (lvl * random.randint(1, 10)), 0)
         right(angle)
         forward(-size)
-yaxis(44, 10)
+yaxis(120, 5) # with lvl = 6 ~ 9 second. lvl = 5 ~ 5 seconds
+
+# get canvas to save as a png
+# https://stackoverflow.com/questions/35629520/convert-images-drawn-by-turtle-to-png-in-python 
+cv = turtle.getcanvas()
+cv.postscript(file='tree_1.eps', colormode='color')
+img = Image.open('tree_1.eps')
+img.save('tree_1.png', 'png')
+
 turtle.done()
+turtle.bye()
